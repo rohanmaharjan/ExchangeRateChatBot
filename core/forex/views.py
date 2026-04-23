5af1eb0157d8222dba9f29389e2911b554afacf1
Rohan Maharjan <rohanmhj9@gmail.com>
2026-04-23 21:05:28 +0545

    Json Response

--- core/forex/views.py
+++ core/forex/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from forex.services import chatbot_response
 
-# Create your views here.
+def chat(request):
+    msg = request.GET.get("msg", "")
+    response = chatbot_response(msg)
+    return JsonResponse({"response": response})
